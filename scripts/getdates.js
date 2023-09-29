367ae1a353da3fa404c952b9ebc8960e8da3277f
Ricardo <rirojas1970@gmail.com>
2023-09-29 20:15:37 -0300

    responsive-menu

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,10 @@
 const modif = document.lastModified;
 document.querySelector(".dat").textContent= `The last modified :${modif}`;
+
+const hamburButton= document.querySelector("#menu");
+const navegation= document.querySelector(".nave");
+
+hamburButton.addEventListener("click", ()=> {
+    navegation.classList.toggle('open');
+    hamburButton.classList.toggle('open');
+})
