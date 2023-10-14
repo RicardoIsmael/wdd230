b9223b8cc6fd2f226c1c0fa34240491ef844d11b
Ricardo <rirojas1970@gmail.com>
2023-10-13 23:30:59 -0300

    chamber-changed

--- chamber/scripts/base.js
+++ chamber/scripts/base.js
@@ -1,7 +1,9 @@
+/*Last Modified code ==========================*/
+
 const modif = document.lastModified;
 document.querySelector(".dat").textContent= `The last modified :${modif}`;
 
-
+/*Hamburger Menu code ==========================*/
 const opened= document.querySelector(".menu");
 const clos= document.querySelector(".nave");
 
@@ -9,3 +11,22 @@ opened.addEventListener("click", () =>{
     clos.classList.toggle('open');
     opened.classList.toggle('open');
 })
+
+/*Dark mode code ================================*/
+const darkbutton= document.querySelector(".change");
+const main= document.querySelector("main");
+
+darkbutton.addEventListener("click", ()=>{
+    if(darkbutton.textContent.includes("☀")){
+        main.style.background= "#000";
+        main.style.color= "#fff";
+        darkbutton.textContent= "🌙";
+        darkbutton.style.color= "white"    
+    }
+    else{
+        main.style.background= "#fff";
+        main.style.color="#000";
+        darkbutton.textContent="☀";
+    }
+})
+
