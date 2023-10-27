041b0a81e74846efaaf3015d0d21107e598590e6
Ricardo <rirojas1970@gmail.com>
2023-10-26 22:47:38 -0300

    home-page-visits

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -8,3 +8,18 @@ hamburButton.addEventListener("click", ()=> {
     navegation.classList.toggle('open');
     hamburButton.classList.toggle('open');
 })
+
+/*code of Number of Visits++++++++++++++++++++++++ */
+
+const displayVisit = document.querySelector('.nVisit')
+let numberVisits = Number(window.localStorage.getItem('visits') || 0);
+if(numberVisits !== 0){
+    displayVisit.textContent= numberVisits;
+
+}else{
+    displayVisit.textContent = "Welcome. This is your first visit..!"
+    
+}
+numberVisits++;
+
+localStorage.setItem('visits',numberVisits );
